763fd2e1b7128c078396a0c5190a3d1e83e32b21
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-12-24 12:48:09 -0300

    initial setup

---
+++ apps/backend/vitest.config.e2e.ts
@@ -0,0 +1,22 @@
+
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './',
+  },
+  plugins: [
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+  resolve: {
+    alias: {
+      'src': resolve(__dirname, './src'),
+    },
+  },
+});
---
+++ apps/backend/vitest.config.ts
@@ -0,0 +1,22 @@
+
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+import { resolve } from 'path';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: './',
+    include: ['src/**/*.spec.ts'],
+  },
+  plugins: [
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+  resolve: {
+    alias: {
+      'src': resolve(__dirname, './src'),
+    },
+  },
+});
